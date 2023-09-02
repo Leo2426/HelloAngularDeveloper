b0f1029f9a61c4b4dd1518d7ee30b1d16dbf9e11
Leonardo Taype Fernandez <93153931+Leo2426@users.noreply.github.com>
2023-09-01 20:01:21 -0500

    feat: change tittle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello Angular developer app I am here';
+  title = 'Hello Angular developer app, my name is Leonardo';
 }
